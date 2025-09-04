6f05f2654114655ade92751f1e604ab71c7e19d5
dimpho290 <dimphotshid1@gmail.com>
2025-09-05 00:09:07 +0200

    Added a fetchNews branch using promise

--- src/promiseVersion/promiseVersion.ts
+++ src/promiseVersion/promiseVersion.ts
@@ -1,11 +1,24 @@
 import "dotenv/config";
-
+import { fetchNews } from "./fetchNews";
 import { fetchWeather } from "./fetchWeather";
 
 const weatherApiKey = process.env.VITE_WEATHER_API_KEY as string;
+const newsApiKey = process.env.VITE_NEWS_API_KEY as string;
 
-fetchWeather(weatherApiKey, "Polokwane").then((weather) => {
-  console.log("City:", weather.name);
-  console.log("Temperature:", weather.temp, "°C");
-  console.log("Weather:", weather.description);
-});
+fetchWeather(weatherApiKey, "Polokwane")
+  .then((weather) => {
+    console.log("City:", weather.name);
+    console.log("Temperature:", weather.temp, "°C");
+    console.log("Weather:", weather.description);
+
+    return fetchNews(newsApiKey);
+  })
+  .then((news) => {
+    console.log("\nHeadlines News:");
+    news.forEach((n, i) => {
+      console.log(`${i + 1}. ${n.description}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Error:", err.message);
+  });
